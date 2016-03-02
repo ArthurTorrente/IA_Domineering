d95731eb9fc31a7a6790344384fe2a4bcdf44509
Kevin WATHTHUHEWA <wathkevin@gmail.com>
2016-03-03 00:40:39 +0100

    Creation Scene Unity
    Création de la Scene
    Création des Assets (Textures + Materials + Camera)
    Création du GameManagerScript
    Création de la classe Square
    Action joueur possible

---
+++ UnityProject/Assets/Scripts/BoardManager.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BoardManager : MonoBehaviour 
+{
+    [SerializeField]
+    int _boardWidth;
+
+    [SerializeField]
+    Camera _mainCamera;
+
+    [SerializeField]
+    GameObject _emptySquare;
+
+    [SerializeField]
+    Material _horizontalMaterial;
+    [SerializeField]
+    Material _verticalMaterial;
+
+    [SerializeField]
+    int _layerEmpty;
+    [SerializeField]
+    int _layerHorizontal;
+    [SerializeField]
+    int _layerVertical;
+
+    bool _horizontalPlayerTurn = true;
+
+    int _nbSquareToogle = 0;
+
+    List<Square> _squares;
+
+	// Use this for initialization
+	void Start () 
+    {
+        CreateBoard();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        PlayerMove();
+	}
+
+    void CreateBoard()
+    {
+        _squares = new List<Square>();
+
+        GameObject gameObject;
+        Vector3 position = Vector3.zero;
+        Quaternion rotation = Quaternion.identity;
+
+        for (int i = 0; i < _boardWidth; ++i)
+        {
+            for(int j = 0; j < _boardWidth; ++j)
+            {
+                position.Set(j, 0.0f, i);
+                
+                gameObject = Instantiate(_emptySquare, position, rotation) as GameObject;
+                gameObject.name = ((i * _boardWidth) + j).ToString();
+
+                _squares.Add(new Square(gameObject, true));
+            }
+        }
+
+        gameObject = _squares[(_boardWidth * _boardWidth) - 1].SquareGameObject;
+
+        _mainCamera.transform.position = new Vector3(gameObject.transform.position.x / 2, 100.0f, gameObject.transform.position.z / 2);
+        _mainCamera.orthographicSize = (_boardWidth / 2) + 1;
+    }
+
+    void PlayerMove()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 1000, 1 << _layerEmpty))
+            {
+                GameObject gameObjectHit = hit.collider.gameObject;
+
+                SetEmptySquare(gameObjectHit);
+
+                ++_nbSquareToogle;
+
+                if(_nbSquareToogle >= 2)
+                {
+                    _nbSquareToogle = 0;
+                    _horizontalPlayerTurn = !_horizontalPlayerTurn;
+                }
+            }
+        }
+    }
+
+    void SetEmptySquare(GameObject gameObjectHit)
+    {
+        if (_horizontalPlayerTurn)
+        {
+            gameObjectHit.GetComponent<Renderer>().material = _horizontalMaterial;
+            gameObjectHit.layer = _layerHorizontal;
+        }
+        else
+        {
+            gameObjectHit.GetComponent<Renderer>().material = _verticalMaterial;
+            gameObjectHit.layer = _layerVertical;
+        }
+    }
+}
---
+++ UnityProject/Assets/Scripts/Square.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Square 
+{
+    bool _isEmpty;
+
+    GameObject _squareGameObject;
+
+    public Square(GameObject gameObject, bool isEmpty)
+    {
+        _isEmpty = isEmpty;
+        _squareGameObject = gameObject;
+    }
+
+    public bool IsEmpty
+    {
+        get { return _isEmpty; }
+        set { _isEmpty = value; }
+    }
+
+    public GameObject SquareGameObject
+    {
+        get { return _squareGameObject; }
+        set { _squareGameObject = value; }
+    }
+}
